6fe0fabbc7cb243fc8329b738b4c37692a1fe175
Mallory LeeWong <m.c.leewong@gmail.com>
2020-01-22 12:56:25 -0500

    fix: chart title updates dynamically

--- src/chart-wrapper.js
+++ src/chart-wrapper.js
@@ -17,11 +17,31 @@ class ChartWrapper extends Component {
   }
 
   render() {
+    // Create title that lists all y axis variable names and updates dynamically
+    let maxKeys = 0
+    let idxMaxKeys = 0
+    for(let i = 0; i < this.props.chartData; i++){
+      let obj = this.props.chartData[i]
+      if(Object.keys(obj).length > maxKeys){
+        maxKeys = Object.keys(obj).length
+        idxMaxKeys = i
+      }
+    }
+    let lineNames = Object.keys(this.props.chartData[idxMaxKeys]).slice(1)
+
+    let titleArr = []
+    lineNames.forEach(function(name){
+      titleArr.push(name)
+    })
+    let titleStrFront = titleArr.slice(0, -2).join(', ')
+    let titleStrEnd = titleArr.slice(-2, -1)
+    let titleStr = titleStrFront + ', and ' + titleStrEnd
+
     return (
       <div>
         <Paper className="chart" elevation={4}>
           <div className="title">
-          <Typography variant="h5">b and c</Typography>
+          <Typography variant="h5">{`${titleStr}`}</Typography>
           </div>
           <div ref="chartWrapper" />
         </Paper>
